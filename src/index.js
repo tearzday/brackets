1e959bbf63d7d22b817d630e334a73f2c018a7b5
tearzday <biverdaniil@gmail.com>
2024-10-23 19:10:13 +0300

    feat: basic function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let library = bracketsConfig.reduce(
+    (obj, value) => ((obj[value[0]] = value[1]), obj),
+    {}
+  );
+  console.log(library);
+
+  let stack = [];
+
+  for (let i = 0; i < str.length; i++) {
+    if (library[str[i]]) {
+      stack.push(str[i]);
+    } else {
+      if (stack.length === 0) {
+        return false;
+      }
+
+      if (library[stack[stack.length - 1]] === str[i]) {
+        stack.pop();
+      } else {
+        return false;
+      }
+      //console.log(library[stack[stack.length - 1]] ,str[i])
+    }
+  }
+
+  if (stack.length == 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
